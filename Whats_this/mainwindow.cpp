197b7171debc75db907d8b0f6a36ec39663bb350
Vivek95vs <55971500+Vivek95vs@users.noreply.github.com>
2026-04-30 15:47:24 +0530

    Add files via upload

---
+++ Whats_this/mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QtGui>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->pushButton->setWhatsThis("Good <b>Afternoon</b>");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
