c5f5f9f4512df1b11d64e77c274feabe9cf9c485
Vivek95vs <55971500+Vivek95vs@users.noreply.github.com>
2026-04-30 15:29:07 +0530

    Add files via upload

---
+++ SqlQuery-1/main.cpp
@@ -0,0 +1,54 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QString>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
+#include <QHostInfo>
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString servername="VIVEK-VS-WS";
+    QString dbname="master";
+
+    QSqlDatabase db=QSqlDatabase ::addDatabase("QODBC");
+
+    db.setConnectOptions();
+
+    //QString dsn =("DRIVER={SQL Native Client};SERVER=%1;DATABASE=%2;UID=sa;PWD=;").arg(servername).arg(dbname);
+    QString dsn=("DRIVER={SQL Native Client 11.0};SERVER=%1;DATABASE=%2;Trusted_Connection=Yes;").arg(servername).arg(dbname);
+
+    db.setDatabaseName(dsn);
+
+    if(db.open())
+    {
+        qDebug()<<"Connected to SQL";
+        QSqlQuery qry;
+        if(qry.exec("SELECT * FROM [dbo].[spt_fallback_usg]"))
+        {
+            while(qry.next())
+            {
+                qDebug()<< qry.value(1).toString();
+            }
+        }
+        else
+        {
+            qDebug()<<"Error ="<<db.lastError().text();
+        }
+        qDebug()<<"Closed";
+        db.close();
+    }
+    else
+    {
+        qDebug()<<"Error ="<<db.lastError().text();
+    }
+
+    return a.exec();
+}
+
+
+
